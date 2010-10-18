74725a2f3711c4705777a929f07bf1cfec7f92bd
lydonchandra <lydonchandra@gmail.com>
2010-10-18 04:14:01 -0700

    Lydon Chandra 18-October-2010 Springify to spring-security-2.0.x

--- src/org/springframework/security/jespa/provider/NtlmAuthenticationProvider.java
+++ src/org/springframework/security/jespa/provider/NtlmAuthenticationProvider.java
@@ -1,17 +1,19 @@
 /**
  * NtlmAuthenticationProvider
  * 
- * Project: jespa-spring
+ * Project: jespa-spring2
  * Licence: Apache License, Version 2.0
  * @author Tomek Kuprowski
+ * Changes:
+ * Lydon Chandra 18-October-2010 Springify to spring-security-2.0.x 
  */
 package org.springframework.security.jespa.provider;
 
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.AuthenticationException;
 import org.springframework.security.jespa.authentication.NtlmAuthenticationToken;
+import org.springframework.security.providers.UsernamePasswordAuthenticationToken;
+import org.springframework.security.providers.dao.DaoAuthenticationProvider;
+import org.springframework.security.userdetails.UserDetails;
 
 /**
  * Authentication provider which role is to skip check of password
